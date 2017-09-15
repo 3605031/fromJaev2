2d94065b7febcb6c42e5db3f1536f6b6aaba7c33
Nate <bhhuynh@ucsd.edu>
2017-09-14 21:24:00 -0700

    main page rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import Navbar from "./components/navbar.js"
+import Featured from "./components/featuredProducts.js"
+import NewArrival from "./components/newArrivals.js"
+import Instagram from "./components/Instagram.js"
+import Footer from "./components/Footer.js"
 import './App.css';
-
+	
 class App extends Component {
   render() {
     return (
-      <Navbar/>
+    <div id="page">
+
+        <header>
+      		<Navbar/>
+        </header>
+
+		<section id="home" class="padbot0">		
+			<div className="flexslider top_slider">
+				<ul className="slides">
+					<li className="slide1">	
+					</li>
+				</ul>
+			</div>
+		</section>
+        
+        <Featured/>
+
+        <NewArrival/>
+
+        <hr class="container"/>
+
+        <Instagram/>
+
+        <Footer/>
+    </div>  
     );
   }
 }
---
+++ client/src/components/newArrivals.js
@@ -0,0 +1,184 @@
+import React from "react"
+
+export default class NewArrivals extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+
+	render(){
+		return(
+		// <!-- NEW ARRIVALS -->
+		<section className="new_arrivals padbot50">
+			
+			{/*<!-- CONTAINER -->*/}
+			<div className="container">
+				<h2>new arrivals</h2>
+				
+				{/*<!-- JCAROUSEL -->*/}
+				<div className="jcarousel-wrapper">
+					
+					{/*<!-- NAVIGATION -->*/}
+					<div className="jCarousel_pagination">
+						<a href="javascript:void(0);" className="jcarousel-control-prev" ><i className="fa fa-angle-left"></i></a>
+						<a href="javascript:void(0);" className="jcarousel-control-next" ><i className="fa fa-angle-right"></i></a>
+					</div>{/*<!-- //NAVIGATION -->*/}
+					
+					<div className="jcarousel" data-appear-top-offset='-100' data-animated='fadeInUp'>
+						<ul>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/chubbycat.jpg" alt="" />
+										<div className="open-project-link"><a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a></div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >ChubbyCat</a>
+										<span className="tovar_price">$98.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/seastuff.png" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >Sea Stuff</a>
+										<span className="tovar_price">$78.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/boo.png" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >boo</a>
+										<span className="tovar_price">$88.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/crush.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >mr.crush</a>
+										<span className="tovar_price">$98.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/flowerbutt.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >Flowerbutt</a>
+										<span className="tovar_price">$65.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/nomnom.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >nom nom</a>
+										<span className="tovar_price">$72.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/poodle.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >poodle</a>
+										<span className="tovar_price">$98.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/slowpoke.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >slowpoke</a>
+										<span className="tovar_price">$78.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/weedmon.jpg" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >weedmon</a>
+										<span className="tovar_price">$88.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+							<li>
+								{/*<!-- TOVAR -->*/}
+								<div className="tovar_item_new">
+									<div className="tovar_img">
+										<img src="images/tovar/women/new/bubblehead.png" alt="" />
+										<div className="open-project-link">
+											<a className="open-project tovar_view" href="javascript:void(0);" data-url="!projects/women/1.html" >quick view</a>
+										</div>
+									</div>
+									<div className="tovar_description clearfix">
+										<a className="tovar_title" href="product-page.html" >Bubble Head</a>
+										<span className="tovar_price">$98.00</span>
+									</div>
+								</div>{/*<!-- //TOVAR -->*/}
+							</li>
+						</ul>
+					</div>
+				</div>{/*<!-- //JCAROUSEL -->*/}
+			</div>{/*<!-- //CONTAINER -->*/}
+	{/*<!-- //NEW ARRIVALS -->*/}</section>
+		)
+	}
+}
