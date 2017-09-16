80b5a3e092d1ae818d0ba3ac778e6737270fcef5
mclinch <michael.clinch@gmail.com>
2017-09-16 11:36:25 -0700

    model update

--- models/product.js
+++ models/product.js
@@ -2,14 +2,20 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 //Product's schema
-var schema = new Schema({
+var productSchema = new Schema({
     imgUrl : {type:String, required:true},
     product_ID:{type:Number,required:true},
     product_name: {type:String,required:true},
     product_category: {type:String,required:true},
     description: {type:String,required:false},
     price: {type:Number,required:true},
-    quantity: {type:Number,required:true}
+    quantity: {type:Number,required:true},
+    sku: {type: String, required: true}
 });
 
-module.exports = mongoose.model('Product',schema);
+// Create the product model with the productSchema
+var Product = mongoose.model("product", productSchema)
+
+// Export the model
+module.exports = Product;
+
